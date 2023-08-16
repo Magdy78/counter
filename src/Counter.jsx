3a014c2081793498f9196ee3b6a4487950db454e
Magdy78 <azzam9778@gmail.com>
2023-08-17 01:10:13 +0300

    final commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Counter.css";
+
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [textVisible, setTextVisible] = useState(true);
+
   const handleIncrement = () => {
     setCount(count + 1);
   };
+
   const handleDecrement = () => {
     setCount(count - 1);
+  };
+
+  const handleToggleText = () => {
+    setTextVisible(!textVisible);
   };
 
   return (
@@ -15,39 +23,37 @@ export default function Counter() {
       <div>
         <h1>Welcome to the Counter </h1>
         <br />
-        <div class="card">
-          <div class="tools">
-            <div class="circle">
-              <span class="red box"></span>
+        <div className="card">
+          <div className="tools">
+            <div className="circle">
+              <span className="red box"></span>
             </div>
-            <div class="circle">
-              <span class="yellow box"></span>
+            <div className="circle">
+              <span className="yellow box"></span>
             </div>
-            <div class="circle">
-              <span class="green box"></span>
+            <div className="circle">
+              <span className="green box"></span>
             </div>
           </div>
-          <div class="card__content">
+          <div className="card__content">
             <div className="Items">
-              <h1
-                className="text-center fs-5"
-                style={{ fontSize: "70px", color: "white" }}
-              >
-                The number now is {count}
-              </h1>
-              <div className="d-flex justify-content-center">
-                <button
-                  onClick={handleIncrement}
-                  className="btn btn-success me-3"
+              {textVisible && (
+                <h1
+                  className="text-center fs-5"
+                  style={{ fontSize: "70px", color: "white" }}
                 >
+                  The number now is {count}
+                </h1>
+              )}
+              <div className="d-flex justify-content-center">
+                <button onClick={handleIncrement} className="btn btn-success me-3">
                   Increase
                 </button>
-                <button
-                  onClick={handleDecrement}
-                  className="btn btn-danger ms-3"
-                >  
-                
+                <button onClick={handleDecrement} className="btn btn-danger ms-3">
                   Decrease
+                </button>
+                <button onClick={handleToggleText} className="btn btn-secondary ms-3">
+                  Toggle Text
                 </button>
               </div>
             </div>
